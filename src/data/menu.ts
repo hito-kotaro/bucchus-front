d3ccab007a678473ec6b95d1d0d9b82074cfcaf4
hito-kotaro <tohi.work@gmail.com>
2023-03-05 09:46:26 +0900

    ドリンク画像を追加

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,15 +1,16 @@
 import { MenuType } from "../components/types/OrderType";
 
 export const menu: MenuType[] = [
-  { name: "ビール", image: "beer.png" },
-  { name: "ビール:大", image: "beer.png" },
-  { name: "ハイボール", image: "highball.png" },
-  { name: "ハイボール:大", image: "highball.png" },
-  { name: "焼酎水割り", image: "highball.png" },
-  { name: "焼酎ロック", image: "highball.png" },
-  { name: "日本酒", image: "highball.png" },
-  { name: "赤ワイン", image: "highball.png" },
-  { name: "白ワイン", image: "highball.png" },
+  { name: "ビール", image: "drinks/beer.png" },
+  { name: "ビール:大", image: "drinks/beer_L.png" },
+  { name: "ハイボール", image: "drinks/highball.png" },
+  { name: "ハイボール:大", image: "drinks/highball_L.png" },
+  { name: "焼酎", image: "drinks/shochu.png" },
+  { name: "日本酒", image: "drinks/sake.png" },
+  { name: "ワイン", image: "drinks/wine.png" },
+  { name: "ホッピー", image: "drinks/hoppy.png" },
+  { name: "ホッピー中", image: "drinks/hoppy_naka.png" },
+
   { name: "水", image: "highball.png" },
 ];
 
